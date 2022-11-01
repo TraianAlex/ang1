37eef3fc696fbc4ca486ccd904a0c82b838e9f71
talexandru <talexandru@opusonesolutions.com>
2022-11-01 11:57:43 -0400

    lifecycle hooks part 1

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -15,46 +15,71 @@ export class RoomsComponent implements OnInit {
     availableRooms: 10,
     bookedRooms: 5,
   };
-
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Free Wi-Fi, Tv, Bathroom',
-      price: 100,
-      photos:
-        'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=800&q=60', //'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=800&q=60',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 3.5,
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air COnditioner, Free Wi-Fi, Tv, Bathroom',
-      price: 200,
-      photos: 'https://unsplash.com/photos/oxeCZrodz78', //'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixl',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.5654,
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Pivate Suite',
-      amenities: 'Air COnditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
-      price: 300,
-      photos: 'https://unsplash.com/photos/oxeCZrodz78', //'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixl',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating: 4.7,
-    },
-  ];
+  roomList: RoomList[] = [];
+  selectedRoom!: RoomList;
+  title = 'Room list';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Free Wi-Fi, Tv, Bathroom',
+        price: 100,
+        photos:
+          'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=800&q=60', //'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=800&q=60',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 3.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air COnditioner, Free Wi-Fi, Tv, Bathroom',
+        price: 200,
+        photos: 'https://unsplash.com/photos/oxeCZrodz78', //'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixl',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.5654,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Pivate Suite',
+        amenities: 'Air COnditioner, Free Wi-Fi, Tv, Bathroom, Kitchen',
+        price: 300,
+        photos: 'https://unsplash.com/photos/oxeCZrodz78', //'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixl',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.7,
+      },
+    ];
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+    this.title = 'Rooms List';
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
+    console.log(room);
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Other Room',
+      amenities: 'Free Wi-Fi, Tv, Bathroom',
+      price: 150,
+      photos:
+        'http://images.unsplash.com/photo-1518791841217-8f16f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=800&q=60',
+      checkinTime: new Date('14-Nov-2021'),
+      checkoutTime: new Date('15-Nov-2021'),
+      rating: 4.2,
+    };
+    //this.roomList.push(room);
+    this.roomList = [...this.roomList, room];
   }
 }
