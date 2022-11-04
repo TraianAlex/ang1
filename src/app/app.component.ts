d2dfe3bc9682a8692592f21f78047bbec369434f
talexandru <talexandru@opusonesolutions.com>
2022-11-03 21:18:54 -0400

    2 way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,19 @@ import { RoomsComponent } from './rooms/rooms.component';
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'ang1';
   role = 'user';
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test server name';
+  username = '';
 
   @ViewChild('user', { read: ViewContainerRef }) vcr!: ViewContainerRef;
   @ViewChild('name', { static: true }) name!: ElementRef;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
     this.name.nativeElement.innerText = 'Hilton hotel';
@@ -20,5 +30,13 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     const componentRef = this.vcr.createComponent(RoomsComponent);
     componentRef.instance.numberOfRooms = 11;
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
