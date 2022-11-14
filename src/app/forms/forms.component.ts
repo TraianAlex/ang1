d347f2f5bf14e7546c72d6be1c2ee223fb61f4d9
talexandru <talexandru@opusonesolutions.com>
2022-11-14 14:51:38 -0500

    basic form

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,11 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forms.component.scss'],
 })
 export class FormsComponent implements OnInit {
+  @ViewChild('f', { static: false }) signupForm!: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
