bf39d2b9e67e000382d651beda389c37a739a0f7
Traian Alexandru <victor_traian@yahoo.com>
2023-02-11 16:55:47 -0500

    test

--- src/app/todos/todo-list/todo-list.component.spec.ts
+++ src/app/todos/todo-list/todo-list.component.spec.ts
@@ -20,4 +20,10 @@ describe('TodoListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should add a todo to the list', () => {
+  //   component.addTodo('Test Todo');
+  //   expect(component.todos.length).toBe(1);
+  //   expect(component.todos[0].text).toBe('Test Todo');
+  // });
 });
